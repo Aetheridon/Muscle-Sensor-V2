770dc1883f780cb8f5a34532b1cc26e21ba87ddf
Aetheridon <lewisvr@protonmail.com>
2026-03-03 13:34:29 +0000

    Connects to network and has a visual indicator using onboard LED

--- arduino-sensor/arduino-sensor.ino
+++ arduino-sensor/arduino-sensor.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <WiFi.h>
 
+#include "arduino_secrets.h"
+
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+int status = WL_IDLE_STATUS;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT); // configure onboard LED to take voltage
+
+  while (status != WL_CONNECTED) {
+    status = WiFi.begin(ssid, pass);
+    delay(500);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Visual indicator since board is being powered via external batt
+  if (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 }
