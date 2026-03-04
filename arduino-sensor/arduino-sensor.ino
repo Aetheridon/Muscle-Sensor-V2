88792837e18528dcf16958460891275238ba41c3
Aetheridon <lewisvr@protonmail.com>
2026-03-04 18:55:54 +0000

    Arduino is now UDP server

--- arduino-sensor/arduino-sensor.ino
+++ arduino-sensor/arduino-sensor.ino
@@ -1,11 +1,24 @@
 #include <SPI.h>
 #include <WiFi.h>
+#include <WifiUdp.h>
 
 #include "arduino_secrets.h"
 
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 int status = WL_IDLE_STATUS;
+
+WiFiUDP udp;
+unsigned int port = 8888;
+
+void check_wifi_status() {
+  // Visual indicator since board is being powered via external batt
+  if (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+}
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT); // configure onboard LED to take voltage
@@ -14,13 +27,32 @@ void setup() {
     status = WiFi.begin(ssid, pass);
     delay(500);
   }
+
+  udp.begin(port);
 }
 
 void loop() {
-  // Visual indicator since board is being powered via external batt
-  if (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(LED_BUILTIN, HIGH);
-  } else {
-    digitalWrite(LED_BUILTIN, LOW);
+  check_wifi_status();
+
+  int packetSize = udp.parsePacket();
+
+  if (packetSize) { // packetSize = 0 if no available packets
+    char packetBuffer[255];
+    int len = udp.read(packetBuffer, 255);
+
+    if (len > 0) {
+      packetBuffer[len] = '\0'; // null terminates buffer
+    }
+
+    if (strcmp(packetBuffer, "START") == 0) {
+      IPAddress clientIP = udp.remoteIP();
+      unsigned int clientPort = udp.remotePort();
+
+      for (int i = 0; i < 500; i++) {
+        udp.beginPacket(clientIP, clientPort);
+        udp.write((uint8_t*)&i, sizeof(i));
+        udp.endPacket();
+      }
+    }
   }
 }
